7c5ec1b140fcf15bc654d57f0066775b786945fb
V1SHAL421 <vishalmkumar3@gmail.com>
2025-06-04 22:04:26 +0100

    fix: implement protection for incorrect parameters for decorator factory (try except function)

--- src/utils/safe_run.py
+++ src/utils/safe_run.py
@@ -1,12 +1,21 @@
 from functools import wraps
 from time import sleep
 
-def safe_run(retries=3, delay=10):
+def safe_run(retries: int = 3, delay: int = 10):
     """Decorator factory that involves try/except and retry functionality into inputted function
     Args:
         retries (int): The maximum number of retries to carry out the function
         delay (int): The number of seconds between each retry
     """
+
+    if retries <= 0:
+        raise ValueError("'retries' has to be a positive integer")
+    if delay <= 0:
+        raise ValueError("'delay' has to be a positive integer")
+    
+    if not isinstance(retries, int) or not isinstance(delay, int):
+        raise TypeError("The decorator parameters must be integers")
+
     def decorator(func):
         @wraps(func)
         def wrapper(*args, **kwargs):
