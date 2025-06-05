90686e15a752aa5292bc1aae1f1b112c3bc49d86
V1SHAL421 <70663959+V1SHAL421@users.noreply.github.com>
2025-06-05 11:23:44 +0100

    [Logger] Implement a centralised logger (#5)
    * feat: implement main logger class
    
    * fix: move log format into function parameter
    
    * feat: implement unit tests for main_logger.py
    
    * fix: sort out the src import error
    
    * feat: add log level checks to unit tests
    
    * fix: Clarify message is in log in comments

---
+++ src/utils/main_logger.py
@@ -0,0 +1,39 @@
+import logging
+
+class MainLogger:
+    """
+    Represents the main logger
+
+    Attributes:
+        _instance (MainLogger or None): Instance of the MainLogger or None
+    """
+    _instance = None
+
+    def __new__(cls):
+        """Returns the MainLogger instance if initialised before, else initialises a new MainLogger instance"""
+        if not cls._instance:
+            cls._instance = super().__new__(cls)
+            cls._instance._initialise()
+        return cls._instance
+    
+    def _initialise(self, format: str = "%(asctime)s - %(levelname)s - %(name)s - %(message)s"):
+        """Initialises the logger
+        
+        Args:
+            format (str, optional): The format of the log. Defaults to %(asctime)s - %(levelname)s - %(name)s - %(message)s"
+        """
+        self.logger = logging.getLogger("Logger") # Name the logger as "Logger"
+        self.logger.setLevel(logging.INFO) # Set the logging level to INFO
+        handler = logging.StreamHandler() # Initialise handler
+        formatter = logging.Formatter(format) # Initialise formatter to the format described in the parameter
+        handler.setFormatter(formatter)
+        self.logger.addHandler(handler)
+
+    # 
+    def get_logger(self):
+        """Initialises the logger
+        
+        Returns:
+            logger (Logger): The logger from the logging module
+        """
+        return self.logger
