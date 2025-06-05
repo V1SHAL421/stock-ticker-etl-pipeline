ff43861dc8fff52490297522f281e168871a6b8c
V1SHAL421 <vishalmkumar3@gmail.com>
2025-06-05 16:02:20 +0100

    fix: add logging to fetch_market_data function

--- src/infrastructure/apis/yahoo_finance_api_client.py
+++ src/infrastructure/apis/yahoo_finance_api_client.py
@@ -1,8 +1,15 @@
 import pandas as pd
 import yfinance as yf
-class yfinanceClient:
 
-    def fetch_market_data(ticker: str = "AMZN") -> pd.DataFrame:
+from utils.main_logger import MainLogger
+from utils.safe_run import safe_run
+class yfinanceClient:
+    def __init__(self):
+        self.logger = MainLogger().get_logger()
+
+    @safe_run()
+    def fetch_market_data(self, ticker: str = "AMZN", period_len: str = "1d", recent_interval: str = "1m") -> pd.DataFrame:
         stock_data = yf.Ticker(ticker)
-        # Log stock data ingested
-        return stock_data
+        minute_level_data = stock_data.history(period=period_len, interval=recent_interval)
+        self.logger.info(f"The stock data retrieved from ticker: {ticker} is {minute_level_data}")
+        return minute_level_data
--- src/infrastructure/interfaces/market_data_client.py
+++ src/infrastructure/interfaces/market_data_client.py
@@ -2,5 +2,5 @@ from typing import Protocol
 import pandas as pd
 
 class MarketDataClient(Protocol):
-    def fetch_market_data(ticker: str) -> pd.DataFrame:
+    def fetch_market_data(self, ticker: str) -> pd.DataFrame:
         ...
