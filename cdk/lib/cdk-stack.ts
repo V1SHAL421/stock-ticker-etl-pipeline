760b7c2a102477d769c36fd954890934f8effa72
V1SHAL421 <vishalmkumar3@gmail.com>
2025-06-09 19:35:47 +0100

    fix: remove incorrect lines in requirements.in

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -7,12 +7,25 @@ export class CdkStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // Defining the S3 Bucket
-    const bucket = new Bucket(
+    // Defining the Main Raw S3 Bucket
+    const main_raw_bucket = new Bucket(
       this, // Refers to the stack that the bucket will be deployed to
       "S3Bucket", // ID
       {
         bucketName: "raw-tick-data-bucket",
+        encryption: BucketEncryption.S3_MANAGED,
+        versioned: true,
+        blockPublicAccess: BlockPublicAccess.BLOCK_ALL, // No one can access bucket without permissions
+        removalPolicy: RemovalPolicy.DESTROY // The bucket deletes when the stack is deleted
+      }
+    )
+
+    // Defining the Test Raw S3 Bucket
+    const test_raw_bucket = new Bucket(
+      this, // Refers to the stack that the bucket will be deployed to
+      "S3Bucket", // ID
+      {
+        bucketName: "test-raw-tick-data-bucket",
         encryption: BucketEncryption.S3_MANAGED,
         versioned: true,
         blockPublicAccess: BlockPublicAccess.BLOCK_ALL, // No one can access bucket without permissions
