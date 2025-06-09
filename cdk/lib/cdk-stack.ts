0f84f9dd740196e928881e213cb571beac8b71e5
V1SHAL421 <vishalmkumar3@gmail.com>
2025-06-09 16:44:49 +0100

    feat: deploy raw s3 bucket

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -1,10 +1,24 @@
-import * as cdk from 'aws-cdk-lib';
+import { Stack, StackProps, RemovalPolicy } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import { Bucket, BlockPublicAccess, BucketEncryption } from 'aws-cdk-lib/aws-s3';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
-export class CdkStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+export class CdkStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
+
+    // Defining the S3 Bucket
+    const bucket = new Bucket(
+      this, // Refers to the stack that the bucket will be deployed to
+      "S3Bucket", // ID
+      {
+        bucketName: "raw-tick-data-bucket",
+        encryption: BucketEncryption.S3_MANAGED,
+        versioned: true,
+        blockPublicAccess: BlockPublicAccess.BLOCK_ALL, // No one can access bucket without permissions
+        removalPolicy: RemovalPolicy.DESTROY // The bucket deletes when the stack is deleted
+      }
+    )
 
     // The code that defines your stack goes here
 
