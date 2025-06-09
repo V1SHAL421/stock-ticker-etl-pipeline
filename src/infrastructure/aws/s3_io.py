13c8e72341ba1e8fd8c79ddd3790fa89a5a4d950
V1SHAL421 <vishalmkumar3@gmail.com>
2025-06-09 17:53:36 +0100

    feat: implement write raw data to s3 bucket function

--- src/infrastructure/aws/s3_io.py
+++ src/infrastructure/aws/s3_io.py
@@ -1,5 +1,26 @@
 """Interactions with Amazon S3"""
+from logging import Logger
+from pyspark.sql.dataframe import DataFrame
+from pyspark.sql import SparkSession, functions as F
+import yaml
+
+from infrastructure.spark.spark_session_manager import SparkSessionManager
 
 
-def write_raw_data_to_s3_bucket(df):
-    pass
+def write_raw_data_to_s3_bucket(df: DataFrame, spark_session_manager: SparkSessionManager, logger: Logger):
+    with open("src/config/infra/s3.yaml", 'r') as file:
+        config = yaml.safe_load(file)
+
+    spark_session_manager.get_spark_session()
+
+    raw_s3_bucket_path = config['s3']['raw_bucket']
+
+    df = df.withColumn("zip_mod", F.col("zip_code").cast("int") % 5)
+
+    df.write.option("useS3ListInplementation", "true").option("compression", "snappy").partitionBy("zip_mod").mode("overwrite").parquet(raw_s3_bucket_path)
+    
+    logger.info("The Spark DataFrame has been written to the raw S3 bucket")
+
+    spark_session_manager.stop_spark_session()
+
+
