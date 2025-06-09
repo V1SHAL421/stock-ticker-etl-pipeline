8466cefd06a777064042ba71c49af81fb8d4e9e3
V1SHAL421 <vishalmkumar3@gmail.com>
2025-06-09 14:41:53 +0100

    feat: Setup AWS environment for CDK

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -4,6 +4,7 @@ import { CdkStack } from '../lib/cdk-stack';
 
 const app = new cdk.App();
 new CdkStack(app, 'CdkStack', {
+  env: { account: '881490108348', region: 'eu-west-2'}
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
