1e46ccba59b32bdb63ac5e2b71a33c03107f211a
V1SHAL421 <70663959+V1SHAL421@users.noreply.github.com>
2025-06-11 14:26:57 +0100

    [Glue Workflow] Setup Glue Crawlers (#15)
    * fix: change name of current cdk stack to s3_bucket_stack
    
    * feat: deploy glue workflow stack

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -1,21 +1,18 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib';
-import { CdkStack } from '../lib/cdk-stack';
+import { S3BucketStack } from '../lib/s3-bucket-stack';
+import { GlueWorkflowStack } from '../lib/glue-workflow-stack';
 
 const app = new cdk.App();
-new CdkStack(app, 'CdkStack', {
-  env: { account: '187065639894', region: 'eu-west-2'}
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
 
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
-
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
-
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+const s3_bucket_stack = new S3BucketStack(app, 'S3BucketStack', {
+  env: { account: '187065639894', region: 'eu-west-2'},
+  stackName: 's3-bucket-stack',
+  description: 'Creates the S3 Buckets'
 });
+
+const glue_workflow_stack = new GlueWorkflowStack(app, 'GlueWorkflowStack', {
+  env: { account: '187065639894', region: 'eu-west-2'},
+  stackName: 'glue-workflow-stack',
+  description: 'Creates the Glue Workflow'
+})
