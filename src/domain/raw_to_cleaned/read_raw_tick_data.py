f31156f508027701c7fe28f6144498aab8ec7193
V1SHAL421 <vishalmkumar3@gmail.com>
2025-06-11 17:11:34 +0100

    feat: implement raw-to-clean etl pipeline

---
+++ src/domain/raw_to_cleaned/read_raw_tick_data.py
@@ -0,0 +1,8 @@
+from pyspark.sql import SparkSession
+
+from utils.safe_run import safe_run
+
+@safe_run()
+def read_raw_tick_data(spark: SparkSession, filepath: str):
+    parquet_df = spark.read.parquet(filepath)
+    return parquet_df
--- src/infrastructure/aws/s3_io.py
+++ src/infrastructure/aws/s3_io.py
@@ -11,7 +11,7 @@ from utils.safe_run import safe_run
 
 
 @safe_run()
-def load_s3_file_path(path="src/config/infra/s3.yaml"):
+def load_s3_file_path(path="src/config/infra/s3.yaml", bucket: str = "main_raw_bucket"):
     """Loads the file path for the raw S3 bucket with date partitioning
 
     Args:
@@ -21,7 +21,7 @@ def load_s3_file_path(path="src/config/infra/s3.yaml"):
         - raw_s3_bucket_path_with_date (str): The file path to the raw S3 bucket with date partition"""
     with open(path, "r") as file:
         config = yaml.safe_load(file)
-    raw_s3_bucket_path = config["s3"]["raw_bucket"]
+    raw_s3_bucket_path = config["s3"][bucket]
     raw_s3_bucket_path_with_date = f"{raw_s3_bucket_path}/{date.today().isoformat()}"
     return raw_s3_bucket_path_with_date
 
@@ -53,11 +53,7 @@ def write_raw_data_to_s3_bucket(
     if not s3_filepath.startswith("s3a://"):
         raise ValueError("The S3 file path is of an incorrect format")
 
-    print("Validation Checks Passed")
-
     spark_session_manager.get_spark_session()
-
-    print("Get Spark Session Passed")
 
     """
     The zip code column is partitioned using the modulo operation % 5 to ensure the data is evenly
@@ -81,3 +77,15 @@ def write_raw_data_to_s3_bucket(
     logger.info("The Spark DataFrame has been written to the raw S3 bucket")
 
     spark_session_manager.stop_spark_session()
+
+@safe_run()
+def write_cleaned_data_to_s3(df: DataFrame, s3_filepath: str):
+    if df is None or df.isEmpty():
+        raise ValueError("The Spark DataFrame does not have any data")
+
+    if not s3_filepath.startswith("s3a://"):
+        raise ValueError("The S3 file path is of an incorrect format")
+    
+    df.write.option("useS3ListImplementation", "true").option(
+        "compression", "snappy"
+    ).partitionBy("date_col").mode("overwrite").parquet(s3_filepath)
