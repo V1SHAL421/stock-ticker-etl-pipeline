31a09de8cb2a9d6447a64b45bb14edc3f96b3f0c
V1SHAL421 <vishalmkumar3@gmail.com>
2025-06-11 16:07:40 +0100

    feat: define main entry point for raw-to-clean glue job

--- src/application/run_raw_to_cleaned.py
+++ src/application/run_raw_to_cleaned.py
@@ -1,1 +1,3 @@
 """Conducts the Glue job from the raw S3 zone to the cleaned S3 zone"""
+def run_raw_to_cleaned_pipeline():
+    pass
--- src/interfaces/glue/raw_to_cleaned_main.py
+++ src/interfaces/glue/raw_to_cleaned_main.py
@@ -1,1 +1,13 @@
 """The entry point for the Glue job to clean raw S3 data and load into cleaned zone of S3"""
+import sys
+
+from application.run_raw_to_cleaned import run_raw_to_cleaned_pipeline
+
+
+def main(argv=None):
+    if argv is None:
+        argv = sys.argv
+    run_raw_to_cleaned_pipeline()
+
+if __name__ == "__main__":
+    sys.exit(main(sys.argv))
