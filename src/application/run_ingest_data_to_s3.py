d1a3806a7728463b9088e10598cb2e35ff44f0c0
V1SHAL421 <vishalmkumar3@gmail.com>
2025-06-12 15:15:55 +0100

    feat: implement integration tests for ingesting market data

--- src/application/run_ingest_data_to_s3.py
+++ src/application/run_ingest_data_to_s3.py
@@ -1,17 +1,17 @@
 """Conducts the ingestion of data from Yahoo! Finance API to the raw zone in S3"""
+from logging import Logger
 from infrastructure.apis.yahoo_finance_api_client import YFinanceClient
 from infrastructure.aws.s3_io import load_s3_file_path, write_raw_data_to_s3_bucket
 from infrastructure.spark.spark_session_manager import SparkSessionManager
 from utils.main_logger import MainLogger
 from utils.pandas_into_spark_df import pandas_into_spark_df
+from utils.safe_run import safe_run
 
 
-def run_ingest_data_to_s3():
-    main_logger = MainLogger()
-    logger = main_logger.get_logger()
-    yfinance_client = YFinanceClient(logger)
-    market_data = yfinance_client.fetch_market_data()
-    spark_df = pandas_into_spark_df(market_data)
+@safe_run()
+def run_ingest_data_to_s3(logger: Logger, yfinance_client: YFinanceClient, raw_s3_filepath: str):
     spark_session_manager = SparkSessionManager(logger)
-    raw_s3_filepath = load_s3_file_path()
+    spark_session = spark_session_manager.get_spark_session()
+    market_data = yfinance_client.fetch_market_data()
+    spark_df = pandas_into_spark_df(spark_session, market_data, logger)
     write_raw_data_to_s3_bucket(spark_df, spark_session_manager, logger, raw_s3_filepath)
